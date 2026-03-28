11a135ee5e19fe27abc55d53bf6e6be220f2e254
Shema Jolivet Gislain <shemajolivet@gmail.com>
2026-03-28 18:01:55 +0200

    removed .env in git history

--- src/lib/redux/api/index.ts
+++ src/lib/redux/api/index.ts
@@ -2,27 +2,27 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../store";
 
 export const hrmsApi = createApi({
-	reducerPath: "api",
-	baseQuery: fetchBaseQuery({
-		baseUrl: "http://84.247.172.162:7009/api/v1",
-		prepareHeaders: (headers, { getState }) => {
-			const token = (getState() as RootState).auth.token;
-			if (token) {
-				headers.set("authorization", `Bearer ${token}`);
-			}
-			return headers;
-		},
-	}),
-	tagTypes: [
-		"Company",
-		"User",
-		"Category",
-		"Auth",
-		"Department",
-		"DepartmentReference",
-		"JobTitle",
-		"JobPosting",
-		"Applicant",
-	],
-	endpoints: () => ({}),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_APP_API_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: [
+    "Company",
+    "User",
+    "Category",
+    "Auth",
+    "Department",
+    "DepartmentReference",
+    "JobTitle",
+    "JobPosting",
+    "Applicant",
+  ],
+  endpoints: () => ({}),
 });
